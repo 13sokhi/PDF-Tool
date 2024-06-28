e3aad3b3e0ca42c28b305ea0267c05cd41d65e3c
manparvegithub <manparve@sheridancollege.ca>
2024-06-28 19:43:33 -0400

    logic completely changed. Implemented PyMuPDF instead of pdf2image to convert PDF file into images

--- PDFToImage.py
+++ PDFToImage.py
@@ -1,16 +1,19 @@
-from pdf2image import convert_from_path
 import os
+import fitz
+from CodeGenerator import CodeGenerator
 
 
 class PDFToImage:
-    def convert_to_images(self, path: str, output_path: str, dpi: int = 300):
+    def convert_to_images(self, path: str, output_path: str):
         if not os.path.exists(path):
             raise Exception(f"{path} does NOT exists!")
         elif not os.path.isdir(output_path):
             raise Exception("Output path can ONLY be a directory!")
 
-        pages = convert_from_path(pdf_path=path, dpi=dpi)
-
-        for i, page in enumerate(pages):
-            img_save_path = os.path.join(output_path, f' ({i + 1}).png')
-            page.save(img_save_path, 'png')
+        pdf_doc = fitz.open(path)
+        for i in range(len(pdf_doc)):
+            page = pdf_doc.load_page(i)
+            pix = page.get_pixmap()
+            code = CodeGenerator.generate_code(5)
+            img_save_path = os.path.join(output_path, f' ({i + 1})-{code}.png')
+            pix.save(img_save_path)
